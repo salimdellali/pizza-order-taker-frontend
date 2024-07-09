a7b970c0c6de800f971c22c5334e0da209e9b07e
Salim DELLALI <dellali.salim@gmail.com>
2024-07-09 13:03:53 -0400

    install chatscope ui kit react, set up basic chat UI with Pizza AI, add prettier rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
 import { useState } from "react"
 import reactLogo from "./assets/react.svg"
 import viteLogo from "/vite.svg"
-import "./App.css"
+// import "./App.css"
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css"
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react"
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [typing, setTyping] = useState(false)
+  const [messages, setMessages] = useState([
+    {
+      message: `Hi there! Welcome to our pizza restaurant. What can I get started for you today?
+          We have a variety of pizzas, sides, toppings, and drinks. Let me know what you'd like, and I'll help you with all the details.
+        `,
+      sender: "Pizza AI",
+      direction: "incoming",
+    },
+  ])
+
+  const handleSend = async (message: string) => {
+    const newMessage = {
+      message,
+      sender: "user",
+      direction: "outgoing",
+    }
+
+    const newMessages = [...messages, newMessage] // all the old messages + the new one
+
+    // update our messages state
+    setMessages(newMessages)
+
+    // set typing indicator (chatgpt is typing)
+    setTyping(true)
+
+    // process messages to the backend (send it over and see the response)
+  }
+
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      style={{
+        position: "relative",
+        height: "95vh",
+        width: "50vw",
+        margin: "0 auto",
+      }}
+    >
+      <MainContainer>
+        <ChatContainer>
+          <MessageList
+            typingIndicator={
+              typing ? <TypingIndicator content="Pizza AI is typing" /> : null
+            }
+          >
+            {messages.map((message, index) => (
+              <Message key={index} model={message} />
+            ))}
+          </MessageList>
+          <MessageInput
+            placeholder="Type message here"
+            onSend={handleSend}
+            attachButton={false}
+          />
+        </ChatContainer>
+      </MainContainer>
+    </div>
   )
 }
 
